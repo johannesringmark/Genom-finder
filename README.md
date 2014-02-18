244004c240dabdff15513264fc5b405441f4616b
johannesringmark <johannes.ringmark@gmail.com>
2014-02-18 18:17:06 +0100

    Initial skeleton

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Genom-finder
 ============
 
  My 23andme ISOGG marker checker. 
-This project aims to create a Nodejs web interface for finding out your haplogroup. This is to bee done by checking custom dna markers against ISOGGs homepage in order to allways give up to date information.
+This project aims to create a javascript interface for finding out your haplogroup. This is to bee done by checking custom dna markers against ISOGGs homepage in order to allways give up to date information.
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = 			require('gulp');
+var gutil = 		require('gulp-util');
+var browserify = 	require('gulp-browserify');
+var watch = 		require("gulp-watch");
+var clean = 		require('gulp-clean');
+var concat = 		require("gulp-concat");
+var spawn = 		require('child_process').spawn;
+
+// Common build operation:
+// 	Take main.js, add deps, concatenate into
+// 	`bundle.js` and put in build directory.
+function build() {
+	gulp.src('./lib/client.js')
+		.pipe(browserify())
+		.pipe(concat("client.js"))
+		.pipe(gulp.dest("./build"));
+}
+
+
+// Default task: build
+gulp.task('default', ['build'], function(){
+
+});
+
+gulp.task('test', function () {
+	// Use browser based testing and not a headless WebKit
+	// proxy, since PhantomJS doesn't support IndexedDB as 
+	// of 1.9.x.
+    spawn("open", ["test/runner.html"]);
+});
+
+// Watch source files and use Browserify to handle deps.
+gulp.task('watch', function() {
+	gulp.src("lib/**/*.js").pipe(watch(function(files) {
+		return build();
+	}));
+});
+
+gulp.task('build', ['clean'], function() {
+    build();
+});
+
+gulp.task('clean', function() {
+	gulp.src('./build', {read: false}).pipe(clean());
+});
